9c89863496c8bd5d9ccc7b64ec5d9452db3a0f0c
arahari <s.arahari@ftr.tokyo>
2018-05-23 03:21:19 +0900

    To delete messages with a 🗑 reaction

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const botToken = process.env.DISCORD_BOT_TOKEN;
 const pinEmoji = process.env.DISCORD_BOT_PIN_EMOJI || '📌';
+const wastebasketEmoji = '🗑';
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.on('messageReactionAdd', async (messageReaction, user) => {
-  if (messageReaction.emoji.name !== pinEmoji) {
-    return;
-  }
+async function dmPinnedMessage (messageReaction, user) {
   if (!user.dmChannel) {
     await user.createDM();
   }
-
   let time = new Date(messageReaction.message.createdTimestamp).toISOString().replace(/T/, ' ').replace(/\..+/, '');
   let info = time + '(UTC)' + ' <@' + messageReaction.message.author.id + '> ' + 'in <#' + messageReaction.message.channel.id + '>';
   let message = '[' + info + '] ' + messageReaction.message.content;
@@ -22,7 +15,26 @@ client.on('messageReactionAdd', async (messageReaction, user) => {
   for (const [key, attachment] of messageReaction.message.attachments) {
     attachments.push(attachment.url);
   }
-  user.dmChannel.send(message, {files: attachments});
+  let messageSent = await user.dmChannel.send(message, {files: attachments});
+  messageSent.react(wastebasketEmoji);
+}
+
+function deleteDM(messageReaction, user) {
+  if (messageReaction.message.author === client.user && user !== client.user) {
+    messageReaction.message.delete();
+  }
+}
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('messageReactionAdd', async (messageReaction, user) => {
+  if (messageReaction.emoji.name === pinEmoji) {
+    dmPinnedMessage(messageReaction, user);
+} else if (messageReaction.emoji.name === wastebasketEmoji) {
+    deleteDM(messageReaction, user);
+  }
 });
 
 client.login(botToken);
